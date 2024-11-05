d0b4143e3f4f809c447fdb1fcb4e539e603ee6df
yamch <yamch@DESKTOP-UL6T4JC>
2024-11-06 00:44:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Team.cs
@@ -0,0 +1,37 @@
+﻿public class Team
+{
+    private readonly List<IWorker> _workers;
+
+    public Team()
+    {
+        _workers = new List<IWorker> { new Worker(), new Worker(), new TeamLeader() };
+    }
+
+    public void BuildHouse(House house)
+    {
+        while (!house.IsComplete())
+        {
+            foreach (var worker in _workers)
+            {
+                worker.Work(house);
+                if (house.IsComplete())
+                {
+                    Console.WriteLine("The house is complete!");
+                    DrawHouse();
+                    return;
+                }
+            }
+        }
+    }
+
+    private void DrawHouse()
+    {
+        Console.WriteLine("House drawing:");
+        Console.WriteLine("   _______");
+        Console.WriteLine("  /       \\");
+        Console.WriteLine(" /_________\\");
+        Console.WriteLine(" |   _ _  |");
+        Console.WriteLine(" |  |  |  |");
+        Console.WriteLine(" |__|__|__|");
+    }
+}
---
+++ ConsoleApp11/interfaces.cs
@@ -0,0 +1,11 @@
+﻿// IPart.cs
+public interface IPart
+{
+    bool IsBuilt { get; set; } 
+}
+
+// IWorker.cs
+public interface IWorker
+{
+    void Work(House house); 
+}
